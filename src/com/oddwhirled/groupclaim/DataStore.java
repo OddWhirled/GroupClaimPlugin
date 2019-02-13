3e5d42f3b11b246c3827c95fe329dffe3280571a
oddwhirled <oddwhirled@gmail.com>
2019-02-12 19:22:42 -0800

    massively improved datastore cleanliness and javadoc

--- src/com/oddwhirled/groupclaim/DataStore.java
+++ src/com/oddwhirled/groupclaim/DataStore.java
@@ -48,6 +48,8 @@ public class DataStore {
     private HashMap<String, GroupInfo> groupInfoCache = new HashMap<>();
     //*if any of these are edited we still want to save in storage
 
+    private HashMap<Player, String> pendingInvites = new HashMap<>();
+
     public static DataStore instance() {
         if (instance == null) {
             instance = new DataStore();
@@ -58,30 +60,55 @@ public class DataStore {
     private DataStore() {
     }
 
-    //returns if group was created (false if already existed or if player in group)
+    /**
+     *
+     * @param p the player to invite
+     * @param group the group to invite them to
+     */
+    public void addInvite(Player p, String group) {
+        if (group != null) {
+            group = group.toLowerCase();
+            pendingInvites.put(p, group);
+        }
+    }
+
+    /**
+     *
+     * @param p the player to remove invites for
+     * @return group player was invited to if the player had a pending invite
+     * otherwise null
+     */
+    public String clearInvite(Player p) {
+        return pendingInvites.remove(p);
+    }
+
+    /**
+     *
+     * @param name the group display name
+     * @param leader the player to make group leader
+     * @return true if the group was created otherwise false (i.e. group already
+     * existed or player in a group)
+     */
     public boolean addGroup(String name, Player leader) {
         if (name == null) {
             return false;
         }
-        String displayName = name;
-        name = name.toLowerCase();
-        //if the group isn't online we have to check the file and cache it
-        //in case someone tries that again
-        if (!joinGroup(leader, name)) {
-            return false;
-        }
-        if (!groupInfoCache.containsKey(name)) {
-            groupInfoCache.put(name, fileGetGroupInfo(name));
-        }
-        if (groupInfoCache.get(name) == null) {
-            GroupInfo g = new GroupInfo(displayName, leader.getUniqueId());
-            groupInfoCache.put(name, g);
-            fileUpdateGroupInfo(g, name);
+        //only proceed if this group doesn't exist
+        //then check if the player is already in a group
+        if (getGroupInfo(name) == null && joinGroup(leader, name)) {
+            GroupInfo g = new GroupInfo(name, leader.getUniqueId());
+            String group = name.toLowerCase();
+            groupInfoCache.put(group, g);
+            fileUpdateGroupInfo(g, group);
             return true;
         }
         return false;
     }
 
+    /**
+     *
+     * @param group the group to remove
+     */
     public void removeGroup(String group) {
         if (group == null) {
             return;
@@ -94,13 +121,37 @@ public class DataStore {
         fileRemoveGroup(group);
     }
 
-    //returns if claim was created (false if already claimed or player not in group)
+    /**
+     *
+     * @param p the player to promote
+     * @return true if player is in a group that exists and gets promoted
+     */
+    public boolean setGroupLeader(Player p) {
+        String group = getGroup(p);
+        if (group != null) {
+            GroupInfo g = getGroupInfo(group);
+            if (g != null) {
+                g.leader = p.getUniqueId();
+                fileUpdateGroupInfo(g, group);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     *
+     * @param group the group to claim this chunk
+     * @param chunk the chunk to claim
+     * @return true if the claim was created otherwise false (i.e. already
+     * claimed or player not in a group)
+     */
     public boolean addClaim(String group, Chunk chunk) {
         if (group == null) {
             return false;
         }
         group = group.toLowerCase();
-        
+
         if (getGroup(chunk) == null) {
             chunkCache.put(chunk, group);
             fileUpdateChunkGroup(chunk, group);
@@ -109,9 +160,46 @@ public class DataStore {
         return false;
     }
 
+    /**
+     *
+     * @param chunk the chunk to remove claim from
+     */
     public void removeClaim(Chunk chunk) {
         chunkCache.put(chunk, null);
         fileUpdateChunkGroup(chunk, null);
+    }
+
+    /**
+     *
+     * @param p the player to add to a group
+     * @param group the group to join
+     * @return true if player not already in group and joins
+     */
+    public boolean joinGroup(Player p, String group) {
+        if (group == null) {
+            return false;
+        }
+        group = group.toLowerCase();
+        if (getGroup(p) == null) {
+            playerCache.put(p, group);
+            fileUpdatePlayerGroup(p, group);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     *
+     * @param p
+     * @return true if player not the leader and leaves
+     */
+    public boolean leaveGroup(Player p) {
+        if (!isLeader(p)) {
+            playerCache.put(p, null);
+            fileUpdatePlayerGroup(p, null);
+            return true;
+        }
+        return false;
     }
 
     public String getGroup(Chunk chunk) {
@@ -129,70 +217,55 @@ public class DataStore {
     }
 
     private GroupInfo getGroupInfo(String name) {
+        if (name == null) {
+            return null;
+        } else {
+            name = name.toLowerCase();
+        }
         if (!groupInfoCache.containsKey(name)) {
             groupInfoCache.put(name, fileGetGroupInfo(name));
         }
         return groupInfoCache.get(name);
     }
 
-    //leader may not be online therefore can't return a player
-    public UUID getGroupLeaderUUID(String group) {
-        if (group == null) {
+    /**
+     *
+     * @param group the group to get the display name
+     * @return this groups display name or null if group doesn't exist
+     */
+    public String getGroupDisplayName(String group) {
+        GroupInfo g = getGroupInfo(group);
+        if (g == null) {
             return null;
         }
-        group = group.toLowerCase();
-        if (!groupInfoCache.containsKey(group)) {
-            groupInfoCache.put(group, fileGetGroupInfo(group));
-        }
-        return groupInfoCache.get(group).leader;
+        return g.displayName;
     }
 
+    /**
+     * The leader may not be online therefore we can't return a player
+     *
+     * @param group the group to get the leader
+     * @return this groups leader UUID or null if group doesn't exist
+     */
+    public UUID getGroupLeaderUUID(String group) {
+        GroupInfo g = getGroupInfo(group);
+        if (g != null) {
+            return null;
+        }
+        return g.leader;
+    }
+
+    /**
+     *
+     * @param p player to check
+     * @return true if the player is in a group and leader of that group
+     */
     public boolean isLeader(Player p) {
         GroupInfo g = getGroupInfo(getGroup(p));
+        if (g == null) {
+            return false;
+        }
         return (g.leader.equals(p.getUniqueId()));
-    }
-
-    public String getGroupDisplayName(String group) {
-        if (group == null) {
-            return null;
-        }
-        group = group.toLowerCase();
-        if (!groupInfoCache.containsKey(group)) {
-            groupInfoCache.put(group, fileGetGroupInfo(group));
-        }
-        return groupInfoCache.get(group).displayName;
-    }
-
-    //returns true if player is in a group and gets promoted
-    public boolean setGroupLeader(Player p) {
-        String group = getGroup(p);
-        if (group != null) {
-            GroupInfo g = groupInfoCache.get(group);
-            g.leader = p.getUniqueId();
-            fileUpdateGroupInfo(g, group);
-            return true;
-        }
-        return false;
-    }
-
-    //returns true if player not already in group and joins
-    public boolean joinGroup(Player p, String group) {
-        if (getGroup(p) == null) {
-            playerCache.put(p, group);
-            fileUpdatePlayerGroup(p, group);
-            return true;
-        }
-        return false;
-    }
-
-    //returns true if player not the leader and leaves
-    public boolean leaveGroup(Player p) {
-        if (!isLeader(p)) {
-            playerCache.put(p, null);
-            fileUpdatePlayerGroup(p, null);
-            return true;
-        }
-        return false;
     }
 
     //Simple un-caching methods that may be called on unload events or on schedule
@@ -202,6 +275,7 @@ public class DataStore {
 
     public void unCache(Player p) {
         playerCache.remove(p);
+        pendingInvites.remove(p);
     }
 
     public void unCache(String g) {
@@ -230,20 +304,11 @@ public class DataStore {
     }
 
     private GroupInfo fileGetGroupInfo(String name) {
-//        for (String s : groupSave.keySet()) {
-//            if (s.equals(name)) {
         return groupSave.get(name);
-//            }
-//        }
     }
 
     private String fileGetPlayerGroup(UUID uuid) {
-//        for (UUID u : playerSave.keySet()) {
-//            if (uuid.equals(u)) {
         return playerSave.get(uuid);
-//            }
-//        }
-//        return null;
     }
 
     //save
@@ -302,8 +367,8 @@ public class DataStore {
 
     private void fileRemovePlayer(Player p) {
         UUID uuid = p.getUniqueId();
-        for(UUID u : playerSave.keySet()) {
-            if(uuid.equals(u)) {
+        for (UUID u : playerSave.keySet()) {
+            if (uuid.equals(u)) {
                 playerSave.remove(u);
             }
         }
